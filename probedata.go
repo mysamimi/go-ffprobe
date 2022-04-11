d809b6d55d898a330178713d8d84ef37ecbdbcb9
Mohammad Samimi <m.samimi@sabaidea.com>
2022-04-12 03:52:18 +0430

    add field_order & rotation

--- probedata.go
+++ probedata.go
@@ -54,41 +54,43 @@ type FormatTags struct {
 // Stream is a json data structure to represent streams.
 // A stream can be a video, audio, subtitle, etc type of stream.
 type Stream struct {
-	Index              int               `json:"index"`
-	ID                 string            `json:"id"`
-	CodecName          string            `json:"codec_name"`
-	CodecLongName      string            `json:"codec_long_name"`
-	CodecType          string            `json:"codec_type"`
-	CodecTimeBase      string            `json:"codec_time_base"`
-	CodecTagString     string            `json:"codec_tag_string"`
-	CodecTag           string            `json:"codec_tag"`
-	RFrameRate         string            `json:"r_frame_rate"`
-	AvgFrameRate       string            `json:"avg_frame_rate"`
-	TimeBase           string            `json:"time_base"`
-	StartPts           int               `json:"start_pts"`
-	StartTime          string            `json:"start_time"`
-	DurationTs         uint64            `json:"duration_ts"`
-	Duration           string            `json:"duration"`
-	BitRate            string            `json:"bit_rate"`
-	BitsPerRawSample   string            `json:"bits_per_raw_sample"`
-	NbFrames           string            `json:"nb_frames"`
-	Disposition        StreamDisposition `json:"disposition,omitempty"`
-	Tags               StreamTags        `json:"tags,omitempty"`
-	Profile            string            `json:"profile,omitempty"`
-	Width              int               `json:"width"`
-	Height             int               `json:"height"`
-	HasBFrames         int               `json:"has_b_frames,omitempty"`
-	SampleAspectRatio  string            `json:"sample_aspect_ratio,omitempty"`
-	DisplayAspectRatio string            `json:"display_aspect_ratio,omitempty"`
-	PixFmt             string            `json:"pix_fmt,omitempty"`
-	Level              int               `json:"level,omitempty"`
-	ColorRange         string            `json:"color_range,omitempty"`
-	ColorSpace         string            `json:"color_space,omitempty"`
-	SampleFmt          string            `json:"sample_fmt,omitempty"`
-	SampleRate         string            `json:"sample_rate,omitempty"`
-	Channels           int               `json:"channels,omitempty"`
-	ChannelLayout      string            `json:"channel_layout,omitempty"`
-	BitsPerSample      int               `json:"bits_per_sample,omitempty"`
+	Index              int                  `json:"index"`
+	ID                 string               `json:"id"`
+	CodecName          string               `json:"codec_name"`
+	CodecLongName      string               `json:"codec_long_name"`
+	CodecType          string               `json:"codec_type"`
+	CodecTimeBase      string               `json:"codec_time_base"`
+	CodecTagString     string               `json:"codec_tag_string"`
+	CodecTag           string               `json:"codec_tag"`
+	RFrameRate         string               `json:"r_frame_rate"`
+	AvgFrameRate       string               `json:"avg_frame_rate"`
+	TimeBase           string               `json:"time_base"`
+	StartPts           int                  `json:"start_pts"`
+	StartTime          string               `json:"start_time"`
+	DurationTs         uint64               `json:"duration_ts"`
+	Duration           string               `json:"duration"`
+	BitRate            string               `json:"bit_rate"`
+	BitsPerRawSample   string               `json:"bits_per_raw_sample"`
+	NbFrames           string               `json:"nb_frames"`
+	Disposition        StreamDisposition    `json:"disposition,omitempty"`
+	Tags               StreamTags           `json:"tags,omitempty"`
+	Profile            string               `json:"profile,omitempty"`
+	Width              int                  `json:"width"`
+	Height             int                  `json:"height"`
+	HasBFrames         int                  `json:"has_b_frames,omitempty"`
+	SampleAspectRatio  string               `json:"sample_aspect_ratio,omitempty"`
+	DisplayAspectRatio string               `json:"display_aspect_ratio,omitempty"`
+	PixFmt             string               `json:"pix_fmt,omitempty"`
+	Level              int                  `json:"level,omitempty"`
+	ColorRange         string               `json:"color_range,omitempty"`
+	ColorSpace         string               `json:"color_space,omitempty"`
+	FieldOrder         string               `json:"field_order,omitempty"`
+	SampleFmt          string               `json:"sample_fmt,omitempty"`
+	SampleRate         string               `json:"sample_rate,omitempty"`
+	Channels           int                  `json:"channels,omitempty"`
+	ChannelLayout      string               `json:"channel_layout,omitempty"`
+	BitsPerSample      int                  `json:"bits_per_sample,omitempty"`
+	SideDataList       []StreamSideDataList `json:"side_data_list,omitempty"`
 }
 
 // StreamDisposition is a json data structure to represent stream dispositions
@@ -114,6 +116,13 @@ type StreamTags struct {
 	Title        string `json:"title,omitempty"`
 	Encoder      string `json:"encoder,omitempty"`
 	Location     string `json:"location,omitempty"`
+}
+
+// StreamSideDataList is a json data structure to represent stream side_data_list
+type StreamSideDataList struct {
+	SideDataType  string `json:"side_data_type,string,omitempty"`
+	DisplayMatrix string `json:"displaymatrix,string,omitempty"`
+	Rotation      int    `json:"rotation,omitempty"`
 }
 
 // StartTime returns the start time of the media file as a time.Duration
